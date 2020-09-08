2a15a8f075889b52fbeb9cb8bfa2edc81d1d72f7
Mohan <mohandidugu333@gmail.com>
2020-09-08 19:22:34 +0530

    10

--- src/lab/exp9/simulation/JS/recursion.js
+++ src/lab/exp9/simulation/JS/recursion.js
@@ -24,7 +24,7 @@ window.view = {
 		 else if ( isNaN(input) === true )
 			alert('Number of disks must be an integer value !')
 		 else if ( input > 10 || input % 1 !== 0 )
-			alert('Only integral value (Greater than equal to 1 and less than equal to 5) is accepted !')
+			alert('Only integral value (Greater than equal to 1 and less than equal to 10) is accepted !')
 		 else {
 			this.disableElement('btnOk')
 			this.enableElement('btnStart')
